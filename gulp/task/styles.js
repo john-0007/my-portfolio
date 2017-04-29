fe4b60e80b113e4e16b77f07c3b0b1cfc69c5b7e
John <Xohnjoseph@gmail.com>
2017-04-30 03:20:53 +0700

    responsive part has been done

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -3,12 +3,13 @@ var gulp=require("gulp"),
     autoprefixer=require("autoprefixer"),
     cssvar=require("postcss-simple-vars"),
     nested=require("postcss-nested"),
-    cssimport=require("postcss-import");
+    cssimport=require("postcss-import"),
+    mixins=require("postcss-mixins");
 
 
 gulp.task("styles",function(){
     return gulp.src("./app/assets/styles/styles.css")
-          .pipe(postcss([cssimport,cssvar,nested,autoprefixer]))
+          .pipe(postcss([cssimport,mixins,cssvar,nested,autoprefixer]))
           .on("error",function(errorinfo){
                console.log(errorinfo.toString());
                this.emit("end");
